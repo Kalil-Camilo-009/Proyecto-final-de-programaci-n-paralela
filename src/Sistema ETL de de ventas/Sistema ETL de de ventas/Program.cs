4cd5fdff396c2f0611817bdf1406c96905d95605
Kalil-Camilo-009 <kalilcamilo205@gmail.com>
2026-07-29 19:59:16 -0400

    Una pequeña actualización

---
+++ src/Sistema ETL de de ventas/Sistema ETL de de ventas/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sistema_ETL_de_de_ventas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Estamos comenzado a trabajar en el sistema ETL");
+        }
+    }
+}
