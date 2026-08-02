743a55db9438fa6131557a6ffa2f0dcee9fa29f0
Ranniel Muñoz <raniel.ant0880@gmail.com>
2026-08-02 11:19:06 -0400

    dpmingo 2 de gosto

--- src/Sistema ETL de de ventas/Sistema ETL de de ventas/Program.cs
+++ src/Sistema ETL de de ventas/Sistema ETL de de ventas/Program.cs
@@ -10,7 +10,7 @@ namespace Sistema_ETL_de_de_ventas
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("vamos comenzado a trabajar en el sistema ETL, ya resolvi mi problema, Recuerden siempre hacer un pull antes de programar y avisar por el grupo, pra que progarmen encima del ultimo cambio funcional, Ya hice mi tarea, del docx word ");
+            Console.WriteLine("vamos comenzado a trabajar en el sistema ETL, ya resolvi mi problema, Recuerden siempre hacer un pull antes de programar y avisar por el grupo, pra que progarmen encima del ultimo cambio funcional, Ya hice mi tarea, del docx word , ready");
         }
     }
 }
